361f80d7a6a438f2cd6fcfa10a0202a6e0706472
Zeref-Igris <igriszeref.002@gmail.com>
2022-08-11 16:48:42 +0000

    Welcome to Project404
    Signed-off-by: Zeref-Igris <igriszeref.002@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_RMX2061.mk
+    $(LOCAL_DIR)/p404_RMX2061.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_RMX2061-user \
-    derp_RMX2061-userdebug \
-    derp_RMX2061-eng
+    p404_RMX2061-user \
+    p404_RMX2061-userdebug \
+    p404_RMX2061-eng
--- p404_RMX2061.mk
+++ p404_RMX2061.mk
@@ -15,16 +15,17 @@ TARGET_BOOT_ANIMATION_RES := 1080
 WITH_GMS := true
 
 # Official
-DERP_BUILDTYPE := Official
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-USE_LEGACY_BOOTANIMATION := true
 
-# Inherit some common stuff
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+# Inherit some common 404 stuff
+$(call inherit-product, vendor/404/configs/common.mk)
+
+# Shinka
+P404_BUILDTYPE := SHINKA
 
 # Device identifier
-PRODUCT_NAME := derp_RMX2061
+PRODUCT_NAME := p404_RMX2061
 PRODUCT_DEVICE := RMX2061
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 6 Pro
